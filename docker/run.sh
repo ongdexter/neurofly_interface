75051f5174916e78ec9a9d22da2082130ffbbd07
Dexter <dexterong94@gmail.com>
2025-04-14 19:33:08 -0400

    Working noetic docker

--- docker/run.sh
+++ docker/run.sh
@@ -1,12 +1,11 @@
  #!/bin/bash
 docker run --privileged \
     -e DISPLAY \
-    -v /dev/bus/usb:/deb/bus/usb \
+    -v /dev:/dev \
     -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
     -v /home/${USER}/.Xauthority:/home/${USER}/.Xauthority \
     -v ~/.ssh:/root/.ssh \
     -v /home/${USER}:/home/${USER} \
     -it --ipc=host --net=host --gpus all \
     --name neurofly_interface \
-    --rm \
     neurofly-interface
